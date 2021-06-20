fbde83528765be31e3997d676c37c950da20af75
Roland Levy <38256994+rolandjlevy@users.noreply.github.com>
2021-06-20 22:28:31 +0000

    Added axios and thunk

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const redux = require('redux');
-const {logger} = require('redux-logger');
-
 const { createStore, applyMiddleware } = redux;
+const { logger } = require('redux-logger');
+const thunkMiddleware = require('redux-thunk').default;
+const middlewares = [logger, thunkMiddleware];
 
+
+// Action types
 const INC_COUNTER = 'INC_COUNTER';
 const DEC_COUNTER = 'DEC_COUNTER';
 
+// Action creators
 const incCounter = () => ({ type: INC_COUNTER });
 const decCounter = () => ({ type: DEC_COUNTER });
 
+// Reducer
 const initialState = { counter: 0 }
 
 const reducer = (state = initialState, action) => {
@@ -28,6 +33,9 @@ const reducer = (state = initialState, action) => {
   }
 }
 
-const store = createStore(reducer, applyMiddleware(logger));
+// Set up the store
+const store = createStore(reducer, applyMiddleware(...middlewares));
+
+const url = 'https://jsonplaceholder.typicode.com/users';
 
 store.dispatch(incCounter());
